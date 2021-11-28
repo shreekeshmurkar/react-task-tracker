d01292fe4c2bad0a144c4e0095ed5bb0a68508bf
shreekeshmurkar <shreekeshmurkar@gmail.com>
2021-11-28 17:17:49 +0530

    added code for task tracker

---
+++ src/App.js
@@ -0,0 +1,103 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Tasks from './components/Tasks';
+import About from './components/About';
+import { BrowserRouter as Rounter, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import AddTask from './components/AddTask';
+
+function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTask = async () => {
+      const taskFromServer = await fetchTasks()
+      setTasks(taskFromServer)
+    }
+    getTask()
+  }, [])
+
+  // fetch tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+    return data
+  }
+
+  // fetch task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+    return data
+  }
+
+  // Add Task
+  const addTask = async (task) => {
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newTask = { id, ...task }
+    // setTasks([...tasks, newTask])
+
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+  }
+
+  // Delete task
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE'
+    })
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
+
+  // Toggle reminder
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updTask)
+    })
+    const data = await res.json()
+
+    setTasks(tasks.map((task) =>
+      task.id === id ? { ...task, reminder: data.reminder } : task
+    )
+    )
+  }
+
+  return (
+    <Rounter>
+      <div className="container">
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+        <Route path="/" exact render={(props) => (
+          <>
+            {
+              showAddTask && <AddTask onAdd={addTask} />
+            }
+            {tasks.length > 0 ?
+              <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> :
+              'No tasks added.'
+            }
+          </>
+        )} />
+        <Route path="/about" component={About} />
+        <Footer />
+      </div>
+    </Rounter>
+  );
+}
+
+export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types'
+
+const Button = ({color, text, onClick}) => {
+    return <button style={{ backgroundColor : color}} className='btn' onClick={onClick}>{text}</button>
+}
+
+Button.defaultProps = {
+    color : 'steelBlue',
+}
+
+Button.propTypes = {
+    text : PropTypes.string,
+    color : PropTypes.string
+}
+
+export default Button
